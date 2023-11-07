c0b1d21b9feeff2ac77a0917ccc98aa3e7caf391
cheemsburger <hornycheems2137@gmail.com>
2023-11-07 19:35:09 +0100

    init

--- client.c
+++ client.c
@@ -1,51 +1,206 @@
+
+#include <arpa/inet.h> 
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
+#include <strings.h> 
 #include <sys/socket.h>
-#include <arpa/inet.h>
-#include <strings.h>
-// "127.0.0.1 7777 UDP 1024 to_send.mp4 receivec.mp4"
-int main (int argc, char* argv[]) {
-    int buff_size = atoi(argv[4]);
-    int handle;
-    char *buffer = (char*) malloc(buff_size*sizeof(char));
-    struct sockaddr_in saddr;
-    int mode = 0;
-    
-    memset(&saddr, 0, sizeof (saddr));
-    saddr.sin_family = AF_INET;
-    inet_pton(AF_INET, argv[1], &(saddr.sin_addr));
-    saddr.sin_port = htons (atoi(argv[2]));
-    socklen_t slen= sizeof(saddr);
+#include <unistd.h> 
+#include <stdbool.h>
+#include <poll.h>
+#include <string.h>
 
-    if (strncmp(argv[3], "UDP", 3) == 0){
-        mode = 0;
-        handle = socket (AF_INET, SOCK_DGRAM, 0);
-        sendto(handle, argv[6], sizeof(argv[6])+10, 0, (struct sockaddr *) &saddr, slen);
-    } else if (strncmp(argv[3], "TCP", 3) == 0){
-        mode = 1;
-        handle = socket(AF_INET, SOCK_STREAM, 0);
-        connect(handle, (struct sockaddr*) &saddr, sizeof(saddr));
-        bzero(buffer, buff_size*sizeof(char));
-        strcpy(buffer, argv[6]);
-        send(handle, buffer, buff_size, 0);
-    } else{ exit(1); }
+#define LISTSIZE 10
+#define BUFFSIZE 50
+#define PORT 7777
+#define SA struct sockaddr
 
-    FILE *file;
-    file = fopen(argv[5],"rb");
-    int n = 0;
-    int counter = 0;
+typedef struct {
+    int offer_id;
+    char item[BUFFSIZE];
+    int time;
+    bool is_active;
+} offer;
 
-    while ( ( n = fread(buffer, 1, buff_size, file) ) > 0 ) {
-        if (mode == 0) { sendto(handle, buffer, n, 0, (struct sockaddr *) &saddr, slen); }
-        if (mode == 1) { send(handle, buffer, n, 0); }
-        counter ++;
+typedef struct {
+
+    int code;
+    char message[BUFFSIZE];
+} message;
+
+int user_mode = 0;
+struct pollfd file_desc[2];
+offer offer_list[LISTSIZE];
+
+void add_offer(message msg) {
+    int my_index = 0;
+    for (int i = 0; i<LISTSIZE; i++) {
+        if (offer_list[i].is_active == false) { my_index = i; }
     }
-    // sendto(handle, buffer, 0, 0, (struct sockaddr *) &saddr, slen);
-    printf("Wyslano %d pakietow o wielkosci %d\n",counter, buff_size);
-    close(handle);
-    fclose(file);
+    char *token = strtok(msg.message, " ");
+    offer_list[my_index].time = atoi(token);
+    token = strtok(NULL," ");
+    offer_list[my_index].offer_id = atoi(token);
+    token = strtok(NULL,"");
+    strncpy(offer_list[my_index].item, token, sizeof(offer_list[my_index].item));
+}
+
+void update_offers() {
+    //
+    while (1) {
+        system("clear");
+        if (user_mode == 1) { printf("1. Przyjmij oferte\n2. Zakoncz program \n"); }
+        else { printf("1. Opublikuj oferte\n2. Zakoncz program \n"); }
+        for (int i = 0; i<LISTSIZE; i++) {
+            if (offer_list[i].is_active == true) {
+                printf("%d  Oferta %d: Zapotrzebowanie na %s\n", offer_list[i].time, offer_list[i].offer_id,  offer_list[i].item);
+                offer_list[i].time -=1;
+                if (offer_list[i].time <= 0) { offer_list[i].is_active = false; }
+            } 
+        }
+        fflush(stdout);
+        sleep(2);
+    }
+}
+void client_mode(int handle) {
+    file_desc[0].fd = 0;    //STDIN
+    file_desc[0].events = POLLIN;
+    file_desc[1].fd = handle; // Socket
+    file_desc[1].events = POLLIN;
+    while ( true ) {
+        int descriptor = poll(file_desc, 2, -1);
+        if (descriptor == -1 ){ exit(1); }
+        for ( int i = 0; i < 2; i++ ) {
+            if ( file_desc[i].revents & POLLIN ) {
+                if (i == 0) { // Input STDIN
+                    char user_input[20];
+                    int n = read(0, user_input, sizeof(user_input));
+                    if ( n>0 ) { user_input[n] = '/0'; }
+                    switch ( atoi(user_input) ) {
+                        case 1: // Send new offer to the server
+                        message msg;
+                        msg.code = 120;
+                        printf("Podaj towar na ktory jest zapotrzebowanie w formacie CZAS-ZASOB: ");
+                        scanf("%s", msg.message);
+                        send(handle, &msg, sizeof(message), 0);
+                        break;
+                        case 2: //Close the program
+                            close(handle);
+                            exit(0);
+                    }
+                }
+                else {
+                    //ODCZYT WIADOMOSCI Z SERWERA
+                }   
+}   }   }   }
+
+void supplier_mode(int handle) { 
+    for (int i = 0; i < LISTSIZE; i++)  //Prepare data to be read via update_offers()
+    { offer_list[i].is_active = false; }
+    //pt_thread_create(update_offers);
+    file_desc[0].fd = 0;    //Set descriptors
+    file_desc[0].events = POLLIN;
+    file_desc[1].fd = handle;
+    file_desc[1].events = POLLIN;
+
+    while (1) {
+        int descriptor = poll(file_desc, 2, -1); //Poll for input. 
+        if (descriptor == -1 ) { exit(1); }
+        for (int i = 0; i < 2; i++) { 
+            if ( file_desc[i].revents & POLLIN ) {
+                if (i == 0) { // Input STDIN
+                    char user_input[20];
+                    int n = read(0, user_input, sizeof(user_input));
+                    if (n>0) {user_input[n] = '/0';}
+                    switch (atoi(user_input)) {
+                    case 1: //Send offer_id to be taken
+                        message to_send;
+                        to_send.code = 200;
+                        scanf("%d", &index);
+                        sprintf(to_send.message, "%ld", index);
+                        send(handle, &to_send, sizeof(message), 0);
+                        break;
+                    case 2: //Close the program
+                        close(handle);
+                        exit(0);
+                    }
+                } else if (i == 1) { // Input from the socket
+                    message mess;
+                    int n = read(handle, &mess, sizeof(message));
+                    switch (mess.code) {
+                        case 11: //NEW_OFFER
+                            add_offer(mess);
+                            break;
+                        case 12: //OFFER_EXPIRED
+                            printf("Oferta nr. %s wygasla", mess.message);
+                                                        // SPRAWDZ ID Z TYMI NA LESCIE USTAW JA NA ACTIVE = FALSEL
+
+                            break;
+                        case 14: //CONNECTION_CLOSED
+                            print("Serwer zakonczyl polaczenie. Zamykam aplikacje");
+                            close(handle);
+                            exit(1);
+                            break;
+                        case 13: // SET_BUSY_TRUE
+                            printf("Uzytkownik zajety. Nie mozna przyjac dwoch zlecen jednoczesnie");
+                            break;
+}   }   }   }   }   }
+
+int login(int handle) {
+    message temp;
+    bzero(&temp, sizeof(message));
+    printf("Podaj dane do logowania: ");
+    scanf("%s", temp.message);
+    temp.code = 10;
+    send(handle, &temp, sizeof(temp), 0);
+    recv(handle, &temp, sizeof(temp), 0); 
+    //Send message with credentials, await respond usermode
+    // and act accordingly
+    switch (temp.code) {
+        case 10: //Invalid credentials
+            return 1;
+            break;
+        case 11: //Client
+            client_mode(1);
+            return 0;
+            break;
+        case 12: //Supplier
+            supplier_mode(1);
+            return 0;
+            break;
+}   }
+
+int main() {
+    int sock, conn_handle; //Establish connection
+    struct sockaddr_in servaddr;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    } else {
+        printf("Socket successfully created..\n");
+    }
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+    if (connect(sock, (SA*)&servaddr, sizeof(servaddr))!= 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    int option = 0;
+    printf("1. Register\n2.Login\n");
+    scanf("%d", &option); //Login to the server
+    switch (option) {
+        case 2:
+            login(conn_handle);
+            break;
+        case 1:
+            reg();
+            break;
+    }
+    close(sock);
     return 0;
 }
+
--- server.c
+++ server.c
@@ -1,95 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <assert.h>
 #include <unistd.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <strings.h>
+#include <stdbool.h>
 #include <fcntl.h>
+#include <string.h>
+#include <time.h>
+#define BUFFSIZE 30
+#define USERSIZE 100
+#define OFFERSIZE 100
 FILE *file;
-// 7070 UDP 1024 name.mp4
-int main (int argc, char* argv[]) {
 
-    struct sockaddr_in udp_saddr, tcp_saddr;
-    struct sockaddr_in udp_caddr, tcp_caddr;
+typedef struct {
 
-    int udp_handle, tcp_handle, tcp_chandle;
+    int code;
+    char message[BUFFSIZE];
+} message;
 
-    int size = 10000*1024;
-    int buff_size = atoi(argv[3]);
-    char *buffer = (char*) malloc(buff_size*sizeof(char));
+typedef struct {
+    int offer_id;
+    int client_id;
+    int supplier_id;
+    char item[BUFFSIZE];
+    int time;
+    bool is_active;
+    bool is_done;
+} offer;
 
-    memset(&udp_saddr, 0, sizeof(udp_saddr));
-    memset(&tcp_saddr, 0, sizeof(tcp_saddr));
-    memset(&udp_caddr, 0, sizeof(udp_caddr));
-    memset(&tcp_caddr, 0, sizeof(tcp_caddr));
-   
-    udp_saddr.sin_family = AF_INET;
-    udp_saddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    udp_saddr.sin_port = htons(atoi(argv[1]));
-    udp_handle = socket(AF_INET, SOCK_DGRAM, 0);
-    bind (udp_handle, (struct sockaddr *) &udp_saddr, sizeof(udp_saddr));
-    setsockopt(udp_handle, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
-    listen(udp_handle, 2);
+typedef struct {
+    int id;
+    bool is_busy;
+} user;
+offer offers[OFFERSIZE];
+user clients[USERSIZE];
+user suppliers[USERSIZE];
 
-    tcp_saddr.sin_family = AF_INET;
-    tcp_saddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    tcp_saddr.sin_port = htons(atoi(argv[1]));
-    tcp_handle = socket(AF_INET, SOCK_STREAM, 0);
-    bind (tcp_handle, (struct sockaddr *) & tcp_saddr, sizeof(tcp_saddr));
-    int status = fcntl(socketfd, F_SETFL, fcntl(socketfd, F_GETFL, 0) | O_NONBLOCK);
-    setsockopt(tcp_handle, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
-    listen(tcp_handle, 2);
-    
-    chandle = accept(handle, (struct sockaddr *) &caddr, &addr_size);
-    sleep(1);
+void update_offers() {
+    while (1) {
+        for (int i = 0; i<OFFERSIZE; i++) {
+            if (offers[i].is_active == true) { 
+                offers[i].time -=1;
+                if (offers[i].time <= 0) { 
+                    offers[i].is_active = false; 
+                }
+            }
+        } 
+        sleep(2);
+    }
+}
+void reopen_file() {
+    fclose(file);
+    file = fopen("userdata.txt", "a+");
+}
 
+void register_user (int handle, message msg) {
+    char *token = strtok(msg.message, " ");
+    char token_buffer[BUFFSIZE];
+    char line_buffer[BUFFSIZE];
+    bzero(token_buffer, sizeof(token_buffer));
+    bzero(line_buffer, sizeof(line_buffer));
+    strcpy(token, token_buffer);
+    int counter = 0;
+    reopen_file();
+    bool is_found;
+    while (fgets(line_buffer, sizeof(line_buffer), file)!=NULL) {
+        if (counter%4 == 0) {
+            for (int i = 0; i<sizeof(line_buffer); i++) {
+                if( line_buffer[i]!=token[i] ) { break; }
+            }
+            }
+        }
+        message
+        bzero(temp_buf, sizeof(temp_buf));
+        counter++;
+    }
 
+    return 0;
+}
 
-
-    int handle, chandle;
-    char *buffer = (char*) malloc(buff_size*sizeof(char));
-    int counter = 0, n = 0; 
-    saadr.sin_family = AF_INET;
-    saadr.sin_addr.s_addr = htonl(INADDR_ANY);
-    saadr.sin_port = htons(atoi(argv[1]));
-    socklen_t clen = sizeof(caddr);
-    socklen_t addr_size;
-    if (strncmp(argv[2],"UDP", 3) == 0){
-        mode = 0;
-        handle = socket(AF_INET, SOCK_DGRAM, 0);
-        bind (handle, (struct sockaddr *) & saadr, sizeof(saadr));
-        listen(handle, 2);
-        setsockopt(handle, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
-        recvfrom(handle, buffer, buff_size, 0, (struct sockaddr *)&caddr, &clen);
-    } else if (strncmp(argv[2],"TCP", 3) == 0) {
-        mode = 1;
-        handle = socket(AF_INET, SOCK_STREAM, 0);
-        bind (handle, (struct sockaddr *) & saadr, sizeof(saadr));
-        setsockopt(handle, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
-        listen(handle, 2);
-        chandle = accept(handle, (struct sockaddr *) &caddr, &addr_size);
-        sleep(1);
-        recv(chandle, buffer, buff_size, 0);
-    } else { exit(1); }
-    file = fopen(buffer, "wb");
-    switch (mode) {
-    case 0:
-        do {        
-            n = recvfrom(handle, buffer, buff_size, 0, (struct sockaddr *)&caddr, &clen);
-            fwrite(buffer, sizeof(char), n, file);
-            counter ++;
-        } while (n == buff_size);
+void user_thread(int my_handle) {
+    message temp;
+    bzero(&temp, sizeof(message));
+    recv(my_handle, &temp, sizeof(message), 0);
+    switch ( temp.code ) {
+    case 10:
+        login();
+    case 20:
+        register_user(my_handle, &temp);
         break;
-    case 1:
-        do {        
-            n = recv(chandle, buffer, buff_size, 0);
-            fwrite(buffer, sizeof(char), n, file);
-            counter ++;
-        } while (n == buff_size);
+
+    
+    default:
         break;
     }
-    fclose(file);
-    close(handle);
-    close(chandle);
-    printf("Odebrano %d pakietow o wielkosci %d\n",counter, buff_size);
+}
+int main(int argc, char **argv) {
+
+    srand(time(NULL));
+    file = fopen("userdata.txt", "a+"); // Initial file open
+
+    int server_handle, client_handle; // Socket setup and initialization
+    struct sockaddr_in server_addr;
+    struct sockaddr_storage client_storage;
+    socklen_t addr_size;
+    bzero(&server_addr, sizeof(server_addr));
+    bzero(&client_storage, sizeof(client_storage));
+    server_handle = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(7777);
+    bind(server_handle, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    listen(server_handle, 100);
+
+    while(1){
+        fflush(stdout);
+        addr_size = sizeof(client_storage);
+        client_handle = accept(server_handle, (struct sockaddr *) &client_storage, &addr_size);
+        user_thread(client_handle);
+        // pthread_create(&user_thread_list[user_count++], NULL, &user_thread, &client_handle);
+        // pthread_join(user_thread_list[user_count], NULL);
+    }
     return 0;
+
 }
