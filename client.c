48c4d9355635a48733f63f36e5e0ca53d209233f
cheemsburger <hornycheems2137@gmail.com>
2023-11-09 00:04:36 +0100

    VIP

--- client.c
+++ client.c
@@ -16,6 +16,7 @@
 #define PORT 7777
 #define SA struct sockaddr
 
+
 typedef struct {
     int offer_id;
     char item[BUFFSIZE];
@@ -28,10 +29,10 @@ typedef struct {
     int code;
     char message[BUFFSIZE];
 } message;
-
+bool timer_paused = false;
 int user_mode = 0;
 int handle;
-struct pollfd file_desc[2];
+struct pollfd fds[2];
 offer offer_list[LISTSIZE];
 
 void add_offer(message msg) {
@@ -47,15 +48,15 @@ void add_offer(message msg) {
     strncpy(offer_list[my_index].item, token, sizeof(offer_list[my_index].item));
 }
 
-void update_offers() {
-    //
+void timer() {
     while (1) {
         system("clear");
-        if (user_mode == 1) { printf("1. Przyjmij oferte\n2. Zakoncz program \n"); }
-        else { printf("1. Opublikuj oferte\n2. Zakoncz program \n"); }
+        while (timer_paused) {}; //Block till stdin is being used. For STDOUT clarity;
+        if (user_mode == 1) { printf("1. Accept offer\n2. Exit \n"); }
+        else { printf("1. Publish offer\n2. Exit \n"); }
         for (int i = 0; i<LISTSIZE; i++) {
             if (offer_list[i].is_active == true) {
-                printf("%d  Oferta %d: Zapotrzebowanie na %s\n", offer_list[i].time, offer_list[i].offer_id,  offer_list[i].item);
+                printf("%d %s %d\n", offer_list[i].offer_id, offer_list[i].item, offer_list[i].time);
                 offer_list[i].time -=1;
                 if (offer_list[i].time <= 0) { offer_list[i].is_active = false; }
             } 
@@ -65,50 +66,104 @@ void update_offers() {
     }
 }
 void client_mode() {
-    file_desc[0].fd = 0;    //STDIN
-    file_desc[0].events = POLLIN;
-    file_desc[1].fd = handle; // Socket
-    file_desc[1].events = POLLIN;
+    // fds[0].fd = 0;    //STDIN
+    // fds[0].events = POLLIN;
+    // fds[1].fd = handle; // Socket
+    // fds[1].events = POLLIN;
     while ( true ) {
-        if ( -1 == poll(file_desc, 2, 0) ){ exit(1); }
-        for ( int i = 0; i < 2; i++ ) {
-            if ( file_desc[i].revents & POLLIN ) {
-                if (i == 0) { // Input STDIN
-                    char user_input[20];
-                    int n = read(0, user_input, sizeof(user_input));
-                    if ( n>0 ) { user_input[n] = '/0'; }
-                    switch ( atoi(user_input) ) {
-                        case 1: // Send new offer to the server
-                        message msg;
-                        msg.code = 120;
-                        printf("Podaj towar na ktory jest zapotrzebowanie w formacie CZAS-ZASOB: ");
-                        scanf("%s", msg.message);
-                        send(handle, &msg, sizeof(message), 0);
-                        break;
-                        case 2: //Close the program
-                            close(handle);
-                            exit(0);
+        // if ( -1 == poll(fds, 2, 0) ){ exit(1); } // Not nesseccary. Synchronous IO
+        // if (fds[0].revents & POLLIN) {
+            int action  = 0;
+            message msg;
+            scanf("%d", &action);
+            switch (action) {
+                case 1: { //Send offer to the server
+                    msg.code = 100; 
+                    timer_paused = true;
+                    printf("Enter offer details. Format TIMEOUT-RESOURCE:  ");
+                    fflush(stdout);
+                    scanf("%s", msg.message);
+                    send(handle, &msg, sizeof(message), 0);
+                    recv(handle, &msg, sizeof(message), 0); // Server result
+                    switch (msg.code) {
+                        case 150: { //Handle offer till the end
+                            add_offer(msg); //Add offer to the timer 
+                            timer_paused = false;
+                            recv(handle, &msg, sizeof(message), 0); //Offer taken by the supplier
+                            timer_paused = true;
+                            recv(handle, &msg, sizeof(message), 0); // Offer completed. End the offer
+                            break;
+                        }
+                        case 250: { //ERROR Offer declined
+                            printf("Offer declined. Try again");
+                            sleep(1);
+                            break;
+                        }
+                        default:
+                            break;    
                     }
                 }
-                else {
-                    //ODCZYT WIADOMOSCI Z SERWERA
-                }   
-}   }   }   }
+                case 2: { // User logout;
+                    msg.code = 500;
+                    send(handle, &msg, sizeof(message), 0);
+                    close(handle);
+                    exit(1); 
+                }
+                default : { break; }
+            // }
+        }
+    }
+}
+
+
+
+
+//         for ( int i = 0; i < 2; i++ ) {
+//             if ( fds[i].revents & POLLIN ) {
+//                 if (i == 0) { // Input STDIN
+//                     char user_input[20];
+//                     int n = read(0, user_input, sizeof(user_input));
+//                     if ( n>0 ) { user_input[n] = '/0'; }
+//                     switch ( atoi(user_input) ) {
+//                         case 1: // Send new offer to the server
+//                         message msg;
+//                         msg.code = 120;
+//                         printf("Podaj towar na ktory jest zapotrzebowanie w formacie CZAS-ZASOB: ");
+//                         scanf("%s", msg.message);
+//                         send(handle, &msg, sizeof(message), 0);
+//                         break;
+//                         case 2: //Close the program
+//                             close(handle);
+//                             exit(0);
+//                     }
+//                 }
+//                 else {
+//                     //ODCZYT WIADOMOSCI Z SERWERA
+//                 }   
+// }   }   }   }
 
 void supplier_mode(int handle) { 
-    for (int i = 0; i < LISTSIZE; i++)  //Prepare data to be read via update_offers()
-    { offer_list[i].is_active = false; }
-    //pt_thread_create(update_offers);
-    file_desc[0].fd = 0;    //Set descriptors
-    file_desc[0].events = POLLIN;
-    file_desc[1].fd = handle;
-    file_desc[1].events = POLLIN;
+
+    //pt_thread_create(timer);
+    fds[0].fd = 0;    //Set descriptors
+    fds[0].events = POLLIN;
+    fds[1].fd = handle;
+    fds[1].events = POLLIN;
 
     while (1) {
-        int descriptor = poll(file_desc, 2, -1); //Poll for input. 
-        if (descriptor == -1 ) { exit(1); }
+        if ( -1 == poll(fds, 2, -1) ) {  // Poll descriptor exit on failure
+            close(handle);
+            exit(1); 
+        }
+        if (fds[0].revents & POLLIN) {
+            
+        }
+        
+
+
+
         for (int i = 0; i < 2; i++) { 
-            if ( file_desc[i].revents & POLLIN ) {
+            if ( fds[i].revents & POLLIN ) {
                 if (i == 0) { // Input STDIN
                     char user_input[20];
                     int n = read(0, user_input, sizeof(user_input));
@@ -170,8 +225,10 @@ int login(int handle) {
             return 0;
             break;
 }   }
+void setup_list();
 
 int main() {
+    setup_list(); //Prepare list with preset values;
     int sock; //Establish connection
     struct sockaddr_in servaddr;
     sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -203,4 +260,13 @@ int main() {
     close(sock);
     return 0;
 }
+void setup_list() {
+    for (int i = 0; i<LISTSIZE; i++ ){
+        offer_list[i].is_active = false;
+        strncpy(offer_list[i].item, "NaN", 3);
+        offer_list[i].time = 0;
+        offer_list[i].offer_id = 0;
+    }
+}
+
 
