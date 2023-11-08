f45d75406593db60e03260b531c4c5c1926208e8
cheemsburger <hornycheems2137@gmail.com>
2023-11-08 01:26:54 +0100

    init

--- client.c
+++ client.c
@@ -10,48 +10,42 @@ struct dzialanie {
     int a;
     int b;
 };
-// "127.0.0.1 7777 UDP 1024 1 2"
+// "
 int main (int argc, char* argv[]) {
-    int buff_size = atoi(argv[4]);
+    char *test = "TCP";
+    int buff_size = 100;
     int handle;
     char *buffer = (char*) malloc(buff_size*sizeof(char));
     struct sockaddr_in saddr;
     int mode = 0;
     struct dzialanie to_send;
-    to_send.a = atoi(argv[5]);
-    to_send.b = atoi(argv[5]);
+    to_send.a = 2;
+    to_send.b = 3;
     memset(&saddr, 0, sizeof (saddr));
     saddr.sin_family = AF_INET;
-    inet_pton(AF_INET, argv[1], &(saddr.sin_addr));
-    saddr.sin_port = htons (atoi(argv[2]));
+    inet_pton(AF_INET, "127.0.0.1", &(saddr.sin_addr));
+    saddr.sin_port = htons (7777);
     socklen_t slen= sizeof(saddr);
 
-    if (strncmp(argv[3], "UDP", 3) == 0){
+    if (strncmp(test, "UDP", 3) == 0){
         mode = 0;
         handle = socket (AF_INET, SOCK_DGRAM, 0);
-        sendto(handle, argv[6], sizeof(argv[6])+10, 0, (struct sockaddr *) &saddr, slen);
-    } else if (strncmp(argv[3], "TCP", 3) == 0){
+    } else if (strncmp(test, "TCP", 3) == 0){
         mode = 1;
         handle = socket(AF_INET, SOCK_STREAM, 0);
         connect(handle, (struct sockaddr*) &saddr, sizeof(saddr));
-        bzero(buffer, buff_size*sizeof(char));
-        strcpy(buffer, argv[6]);
-        send(handle, buffer, buff_size, 0);
     } else{ exit(1); }
 
-    FILE *file;
-    file = fopen(argv[5],"rb");
-    int n = 0;
-    int counter = 0;
-
-    while ( ( n = fread(buffer, 1, buff_size, file) ) > 0 ) {
-        if (mode == 0) { sendto(handle, buffer, n, 0, (struct sockaddr *) &saddr, slen); }
-        if (mode == 1) { send(handle, buffer, n, 0); }
-        counter ++;
-    }
+    // while ( ( n = fread(buffer, 1, buff_size, file) ) > 0 ) {
+        // if (mode == 0) { sendto(handle, buffer, n, 0, (struct sockaddr *) &saddr, slen); }
+        // if (mode == 1) { send(handle, buffer, n, 0); }
+    if (mode == 0) { sendto(handle, &to_send, sizeof(struct dzialanie), 0, (struct sockaddr *) &saddr, slen);}
+    if (mode == 1) { send(handle, &to_send, sizeof(struct dzialanie), 0); }
+    //     counter ++;
+    // }
     // sendto(handle, buffer, 0, 0, (struct sockaddr *) &saddr, slen);
-    printf("Wyslano %d pakietow o wielkosci %d\n",counter, buff_size);
+    // printf("Wyslano %d pakietow o wielkosci %d\n",counter, buff_size);
     close(handle);
-    fclose(file);
+    // fclose(file);
     return 0;
 }
--- server.c
+++ server.c
@@ -3,20 +3,25 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <fcntl.h>
+#include <poll.h>
 
 FILE *file;
 struct dzialanie {
     int a;
     int b;
 };
-// 7070 UDP 1024 name.mp4
-int main (int argc, char* argv[]) {
 
+int main (int argc, char* argv[]) {
+    struct pollfd fds[2];
+    struct dzialanie received;
     struct sockaddr_in udp_saddr, tcp_saddr;
     struct sockaddr_in udp_caddr, tcp_caddr;
+    socklen_t udp_clen, tcp_clen;
 
-    int udp_handle, tcp_handle, tcp_chandle;
+    int udp_handle, tcp_sandle, tcp_chandle = 0, result = 0, retval = 0;
 
     int buff_size = atoi(argv[3]);
     char *buffer = (char*) malloc(buff_size*sizeof(char));
@@ -28,18 +33,72 @@ int main (int argc, char* argv[]) {
 
     udp_saddr.sin_family = AF_INET;
     udp_saddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    udp_saddr.sin_port = htons(atoi(argv[1]));
-    
+    udp_saddr.sin_port = htons(7777);
+    udp_clen = sizeof(udp_caddr);
+    udp_handle = socket(AF_INET, SOCK_DGRAM, 0);
+    bind (udp_handle, (struct sockaddr *) & udp_saddr, sizeof(udp_saddr));
+    listen(udp_handle, 2);
+
     tcp_saddr.sin_family = AF_INET;
     tcp_saddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    tcp_saddr.sin_port = htons(atoi(argv[1]));
-    tcp_handle = socket(AF_INET, SOCK_STREAM, 0);
-    fcntl(tcp_handle, F_SETFL, fcntl(tcp_handle, F_GETFL, 0) | O_NONBLOCK);
-    bind (tcp_handle, (struct sockaddr *) & tcp_saddr, sizeof(tcp_saddr));
-    listen(tcp_handle, 2);
+    tcp_saddr.sin_port = htons(7777);
+    tcp_clen = sizeof(tcp_caddr);
+    tcp_sandle = socket(AF_INET, SOCK_STREAM, 0);
+    fcntl(tcp_sandle, F_SETFL, fcntl(tcp_sandle, F_GETFL, 0) | O_NONBLOCK);
+    bind (tcp_sandle, (struct sockaddr *) & tcp_saddr, sizeof(tcp_saddr));
+    listen(tcp_sandle, 2);
+
+    fds[0].fd = udp_handle;
+    fds[0].events = POLLIN;
+    fds[1].fd = tcp_chandle;
+    fds[1].events = POLLIN;
+    
     while (1)
-    {
-        tcp_chandle = connect()
+    {   
+        tcp_chandle = accept(tcp_sandle, (struct sockaddr *) &tcp_caddr, &tcp_clen);
+        fds[1].fd = tcp_chandle;
+        if (tcp_chandle == -1) { retval = poll(fds, 2, 0); }
+        else { 
+            retval = poll(fds, 2 , 10000);
+            if (!retval) {
+                printf("TCP Timeout!");
+                fflush(stdout);
+                close(tcp_chandle);
+            }
+            if (fds[1].revents & POLLIN) {
+                recv(tcp_chandle, &received, sizeof(struct dzialanie) , 0 ); 
+                result = received.a+received.b;
+                printf("%d", result);
+                send(tcp_chandle, &result, sizeof(int), 0);
+                close(tcp_chandle);
+            }
+        }
+        
+        if (fds[0].revents & POLLIN) {
+            recvfrom(udp_handle, &received, sizeof(struct dzialanie), 0, (struct sockaddr *)&udp_caddr, &udp_clen);
+            result = received.a + received.b;
+            printf("%d", result);
+            sendto(udp_handle, &result, sizeof(int), 0 , (struct sockaddr *) &udp_caddr, udp_clen);
+        }
+        fflush(stdout);
+    }
+    return 0;
+
+
+
+
+
+        // if (tcp_chandle != -1) {
+
+        //     struct dzialanie received;
+        //     while ( 0 > recv(tcp_chandle, &received, sizeof(struct dzialanie) ,0 ) ){
+        //         continue;
+        //     }
+        //     result = received.a+received.b;
+        //     send(tcp_chandle, &result, sizeof(int), 0);
+        //     close(tcp_chandle);
+        // }
+        // if (n = recvfrom(handle, buffer, buff_size, 0, (struct sockaddr *)&caddr, &clen);
     }
     
 
@@ -86,5 +145,3 @@ int main (int argc, char* argv[]) {
     // close(handle);
     // close(chandle);
     // printf("Odebrano %d pakietow o wielkosci %d\n",counter, buff_size);
-    return 0;
-}
